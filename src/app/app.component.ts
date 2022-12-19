b30b2cec5e0547d0d187b68204d0d3ac99b2efc5
jonnahfreire <jonnas.freire17@gmail.com>
2022-12-18 21:55:48 -0300

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Book } from './Book';
+import { BookService } from './services/book/book.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'library';
+export class AppComponent implements OnInit {
+  books!: Book[];
+
+  constructor(private bookService: BookService) {
+    this.loadBooks();
+  }
+  
+  ngOnInit(): void { }
+
+  loadBooks() {
+    this.bookService
+      .getAll()
+      .subscribe(books => this.books = books);
+  }
+
+  getBooksByUserId(id: number): Book[] {
+    const books = this.books?.filter(book => book.idowner === id);
+    return books;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ListBooksComponent } from './components/books/list-books/list-books.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ListBooksComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
