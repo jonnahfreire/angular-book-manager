d911b52b4f2cc6cfa44277762d0d9a9a4222c23e
jonnahfreire <jonnas.freire17@gmail.com>
2022-12-19 10:53:10 -0300

    Create: library-header

---
+++ src/app/components/library-header/library-header.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { LibraryHeaderComponent } from './library-header.component';
+
+describe('LibraryHeaderComponent', () => {
+  let component: LibraryHeaderComponent;
+  let fixture: ComponentFixture<LibraryHeaderComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ LibraryHeaderComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(LibraryHeaderComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
